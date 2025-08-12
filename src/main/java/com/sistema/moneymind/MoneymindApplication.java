20d5c80c214cc46f4380cc8de87536834fc9ddd2
VitinhoChicas <victorchicas525@gmail.com>
2025-08-12 20:18:01 -0300

    iniciando projeto

---
+++ src/main/java/com/sistema/moneymind/MoneymindApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.moneymind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneymindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneymindApplication.class, args);
+	}
+
+}
